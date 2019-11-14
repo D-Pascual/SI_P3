6eb45491ed8ab9ef954266c6d82c284beb0e1f57
D-Pascual <a@al.com>
2019-11-14 19:41:08 +0100

    clase

---
+++ public_html/app/SQL/setPrice.sql
@@ -0,0 +1,9 @@
+--orderdetail rellenar columna price
+--precio actual en price de products
+--fecha del pedido en orderdate de orders
+--
+--estamos en 2019
+  --  -precio ahora: 20eur
+  --  -año pedido: 2015
+  --  -Ha ido aumentando un 2% anualmente
+
