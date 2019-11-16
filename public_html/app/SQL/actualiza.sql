4c3f6cf348e7e82d891469525a702a4eb5b91660
David Pascual <david.pascualh@estudiante.uam.es>
2019-11-16 15:17:42 +0100

    setprice++

--- public_html/app/SQL/actualiza.sql
+++ public_html/app/SQL/actualiza.sql
@@ -1,5 +1,4 @@
 -- Agrupados duplicados
-
 INSERT INTO public.orderdetail
 SELECT orderid, prod_id, price, COUNT(*)
 FROM public.orderdetail
@@ -8,69 +7,43 @@ HAVING COUNT(*) > 1
 
 DELETE FROM public.orderdetail
 WHERE quantity < 2 AND orderid IN (
-SELECT orderid
+    SELECT orderid
     FROM public.orderdetail
     WHERE (orderid, prod_id) IN (
-	SELECT orderid, prod_id
-    FROM public.orderdetail
-    GROUP BY orderid, prod_id
-    HAVING COUNT(*) > 1
-    ORDER BY orderid
-)
-);;
+        SELECT orderid, prod_id
+        FROM public.orderdetail
+        GROUP BY orderid, prod_id
+        HAVING COUNT(*) > 1
+        ORDER BY orderid));
 
 --Añadir primary y foreign key a actormovies 
 ALTER TABLE public.imdb_actormovies
-    ADD CONSTRAINT imdb_actormovies_pkey PRIMARY KEY (actorid, movieid, "character")
-,
-ADD CONSTRAINT imdb_actormovies_actorid_fkey FOREIGN KEY
-(actorid) 
-        REFERENCES public.imdb_actors
-(actorid) MATCH SIMPLE 
-        ON
-UPDATE NO ACTION ON
-DELETE NO ACTION,
-ADD CONSTRAINT imdb_actormovies_movieid_fkey FOREIGN KEY
-(movieid) 
-        REFERENCES public.imdb_movies
-(movieid) MATCH SIMPLE
-        ON
-UPDATE NO ACTION ON
-DELETE NO ACTION;
+ADD CONSTRAINT imdb_actormovies_pkey PRIMARY KEY (actorid, movieid, "character"),
+ADD CONSTRAINT imdb_actormovies_actorid_fkey FOREIGN KEY (actorid) 
+    REFERENCES public.imdb_actors (actorid) MATCH SIMPLE 
+    ON UPDATE NO ACTION ON DELETE NO ACTION, 
+ADD CONSTRAINT imdb_actormovies_movieid_fkey FOREIGN KEY (movieid) 
+    REFERENCES public.imdb_movies (movieid) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 --Añadir foreign key a inventory
 ALTER TABLE public.inventory
-    ADD CONSTRAINT inventory_prod_id_fkey FOREIGN KEY (prod_id) 
-        REFERENCES public.products (prod_id)
-MATCH SIMPLE 
-        ON
-UPDATE NO ACTION ON
-DELETE NO ACTION;
+ADD CONSTRAINT inventory_prod_id_fkey FOREIGN KEY (prod_id) 
+    REFERENCES public.products (prod_id) MATCH SIMPLE 
+    ON UPDATE NO ACTION ON DELETE NO ACTION; 
 
 --Añadir primary y foreign key a orderdetail 
 ALTER TABLE public.orderdetail
-    ADD CONSTRAINT orderdetail_pkey PRIMARY KEY (orderid, prod_id)
-,
-ADD CONSTRAINT orderdetail_orderid_fkey FOREIGN KEY
-(orderid) 
-        REFERENCES public.orders
-(orderid) MATCH SIMPLE 
-        ON
-UPDATE NO ACTION ON
-DELETE NO ACTION,
-ADD CONSTRAINT orderdetail_prod_id_fkey FOREIGN KEY
-(prod_id) 
-        REFERENCES public.products
-(prod_id) MATCH SIMPLE
-        ON
-UPDATE NO ACTION ON
-DELETE NO ACTION;
+ADD CONSTRAINT orderdetail_pkey PRIMARY KEY (orderid, prod_id), 
+ADD CONSTRAINT orderdetail_orderid_fkey FOREIGN KEY (orderid) 
+    REFERENCES public.orders (orderid) MATCH SIMPLE 
+    ON UPDATE NO ACTION ON DELETE NO ACTION, 
+ADD CONSTRAINT orderdetail_prod_id_fkey FOREIGN KEY (prod_id) 
+    REFERENCES public.products (prod_id) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 --Añadir primary y foreign key a orders
 ALTER TABLE public.orders
-    ADD CONSTRAINT orders_customerid_fkey FOREIGN KEY (customerid) 
-        REFERENCES public.customers (customerid)
-MATCH SIMPLE 
-        ON
-UPDATE NO ACTION ON
-DELETE NO ACTION; 
+ADD CONSTRAINT orders_customerid_fkey FOREIGN KEY (customerid) 
+    REFERENCES public.customers (customerid) MATCH SIMPLE 
+    ON UPDATE NO ACTION ON DELETE NO ACTION; 
