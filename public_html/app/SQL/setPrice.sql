915a2bed1bdceea3161d6795a1f04cd2e008b2e4
David Pascual <david.pascualh@estudiante.uam.es>
2019-11-16 17:54:33 +0100

    setPrice hecho

--- public_html/app/SQL/setPrice.sql
+++ public_html/app/SQL/setPrice.sql
@@ -1,4 +1,4 @@
 UPDATE public.orderdetail as OD
-SET price = ROUND(CAST(P.price / power(1.02, extract(year FROM now()) - extract(year FROM O.orderdate)) AS NUMERIC), 2) --products.price / power(1.02, 2019- extract(year FROM orders.orderdate))
+SET price = ROUND(CAST(P.price / power(1.02, extract(year FROM now()) - extract(year FROM O.orderdate)) AS NUMERIC), 2) 
 FROM public.products as P, public.orders as O 
 WHERE OD.prod_id = P.prod_id AND OD.orderid = O.orderid;
